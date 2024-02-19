812554c2e5b3eaa36d60ddbf1318cbadb9f5a65b
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-02-19 14:03:52 +0300

    copied

--- prj.lab/stackarr/stackarr.hpp
+++ prj.lab/stackarr/stackarr.hpp
@@ -3,19 +3,28 @@
 #define STACKARR_HPP
 #include <Complex/Complex.hpp>
 
+
 class StackArr{
  
 public:
-  StackArr();
-  StackArr(const StackArr& rhs);
+  [[nodiscard]] StackArr() ;
+
+  [[nodiscard]] StackArr(const StackArr& rhs);
+
   ~StackArr();
-  [[nodiscard]] StackArr& operator=(const StackArr& rhs);
-  void Push(const Complex& rhs);
-  void Pop() noexcept;
+
+  [[nodiscard]] StackArr& operator=(const StackArr& rhs) ;
+
   bool IsEmpty() const noexcept;
+
+  void Pop() noexcept;
+
+  void Push(const Complex& rhs);
+
   [[nodiscard]] Complex& Top();
+
   [[nodiscard]] const Complex& Top() const;
-  
+
   void Clear() noexcept;
 private:
   std::ptrdiff_t capacity_ = 1000;
