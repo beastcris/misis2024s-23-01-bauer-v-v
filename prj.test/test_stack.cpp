9610c5c3a291524c76a029f7da669d3063297bd1
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-02-19 14:04:18 +0300

    add: copy ctor test

--- prj.test/test_stack.cpp
+++ prj.test/test_stack.cpp
@@ -1,14 +1,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include <stackarr/stackarr.hpp>
-#include <iostream>
-
 
 TEST_CASE("stackarr test") {
   StackArr my_stack;
   CHECK_THROWS(my_stack.Top());
   CHECK(my_stack.IsEmpty());
-
   Complex first_complex(3, 5);
   Complex second_complex(2, 7);
 
@@ -16,17 +13,14 @@ TEST_CASE("stackarr test") {
   my_stack.Push(second_complex);
 
   CHECK_EQ(my_stack.Top(), second_complex);
+  my_stack.Pop();
   CHECK_EQ(my_stack.Top(), first_complex);
 
   Complex third_complex(1, 1);
 
   StackArr my_stack2;
   my_stack2.Push(third_complex);
-  my_stack = my_stack2;
-  CHECK_EQ(my_stack.Top(), third_complex);
-
-  my_stack.Push(third_complex);
-  StackArr a(my_stack);
+  StackArr a(my_stack2);
   CHECK_EQ(a.Top(), third_complex);
 
   Complex fourth_complex(2.3, 7.3);
@@ -44,9 +38,38 @@ TEST_CASE("stackarr test") {
   eq1.Push(second_complex);
   eq1.Push(third_complex);
   eq2 = eq1;
+  CHECK_EQ(eq2.Top(), third_complex);
+  eq1.Pop();
+  eq2.Pop();
   CHECK_EQ(eq1.Top(), eq2.Top());
+  eq1.Pop();
+  eq2.Pop();
   CHECK_EQ(eq1.Top(), eq2.Top());
-  CHECK_EQ(eq1.Top(), eq2.Top());
+  eq1.Pop();
+  eq2.Pop();
   CHECK(eq1.IsEmpty());
   CHECK(eq2.IsEmpty());
+}
+
+TEST_CASE("Top method tests") {
+  Complex compl1 = Complex(2.4, 1.5);
+  StackArr stack1 = StackArr();
+  stack1.Push(compl1);
+  stack1.Top() = Complex(0.0, 0.0);
+  CHECK_EQ(stack1.Top(), Complex(0.0, 0.0));
+}
+
+TEST_CASE("copy ctor") {
+  StackArr a;
+  Complex first_complex(3, 5);
+  Complex second_complex(2, 7);
+
+  a.Push(first_complex);
+  a.Push(second_complex);
+
+  StackArr b(a);
+
+  CHECK_EQ(b.Top(), second_complex);
+  b.Pop();
+  CHECK_EQ(b.Top(), first_complex);
 }
