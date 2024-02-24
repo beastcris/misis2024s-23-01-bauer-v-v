013db253b18928362aea22ae75ba95dbe5e5f65a
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-02-25 00:46:41 +0300

    tested: queue

--- prj.test/test_queue.cpp
+++ prj.test/test_queue.cpp
@@ -22,13 +22,48 @@ TEST_CASE("base") {
 
   a.Push(second_complex);
   a.Push(second_complex);
-  QueueLst b = a;
+  QueueLst b(a);
   CHECK_EQ(a.Top(), first_complex);
   a.Pop();
   CHECK_EQ(a.Top(), second_complex);
   a.Push(first_complex);
   CHECK_EQ(a.Top(), second_complex);
   a.Pop();
+  a.Pop();
+  CHECK_EQ(a.Top(), first_complex);
+
+
+  a.Clear();
+  a.Push(second_complex);
+  a.Push(second_complex);
+  b.Clear();
+  b = a;
+  CHECK_EQ(a.Top(), second_complex);
+  a.Pop();
+  b.Pop();
+  CHECK_EQ(a.Top(), second_complex);
+  a.Push(first_complex);
+  b.Push(second_complex);
+  a.Pop();
+  b.Pop();
+  CHECK((a.Top() == b.Top()) == false);
+
+  a.Push(first_complex);
+  a.Push(first_complex);
+  a.Push(first_complex);
+  a = b;
+  CHECK_EQ(a.Top(), b.Top());
+  b.Clear();
+  CHECK(b.IsEmpty());
+  a.Clear();
+  a.Push(second_complex);
+  a.Push(first_complex);
+  a.Push(first_complex);
+
+
+  CHECK_EQ(a.Top(), second_complex);
+  a.Pop();
+  CHECK_EQ(a.Top(), first_complex);
   a.Pop();
   CHECK_EQ(a.Top(), first_complex);
 }
