a660f842e5004bebc17f6a5028fefd901a18bd16
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-02-26 12:28:06 +0300

    tryna: some lines

--- prj.cw/opencv.cpp
+++ prj.cw/opencv.cpp
@@ -1,10 +1,34 @@
-#include <opencv2/imgproc.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 
 int main() {
-  cv::Mat1b m(200, 200, 127);
-  cv::imshow("example", m);
-  cv::waitKey(0);
+  
+  cv::VideoCapture cap("C:/Users/Huawei/source/repos/opencv/sources/samples/cpp/tutorial_code/calib3d/real_time_pose_estimation/Data/box.mp4");
+
+  if (cap.isOpened() == false)
+  {
+    std::cout << "Cannot open the video file" << std::endl;
+    std::cin.get(); 
+    return -1;
+  }
+
+  
+  cv::String wn = "My first vidoe";
+  cv::namedWindow(wn, cv::WINDOW_NORMAL);
+
+  bool bSuccess = true;
+  while (bSuccess!=false) {
+    cv::Mat frame;
+    bool bSuccess = cap.read(frame);
+
+    cv::imshow(wn, frame);
+
+
+    if (cv::waitKey(10) == 27)
+    {
+      std::cout << "Esc key is pressed by user. Stoppig the video" << std::endl;
+      break;
+    }
+  }
 }
