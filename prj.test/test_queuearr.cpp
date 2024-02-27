7946ed20b1ad9c9f254a9e4e1701190b67fcbd0d
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-02-27 20:56:02 +0300

    tested: operator=, copy ctor and throws

--- prj.test/test_queuearr.cpp
+++ prj.test/test_queuearr.cpp
@@ -34,4 +34,52 @@ TEST_CASE("base") {
   CHECK_EQ(a.Top(), second_complex);
   a.Pop();
   CHECK(a.IsEmpty());
+
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Push(first_complex);
+  a.Push(second_complex);
+  QueueArr b(a);
+
+  while (!a.IsEmpty()) {
+    CHECK_EQ(a.Top(), b.Top());
+    a.Pop();
+    b.Pop();
+  }
+
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Pop();
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Push(first_complex);
+  QueueArr c(a);
+
+  while (!a.IsEmpty()) {
+    CHECK_EQ(a.Top(), c.Top());
+    a.Pop();
+    c.Pop();
+  }
+
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Pop();
+  a.Push(first_complex);
+  a.Push(second_complex);
+  a.Push(first_complex);
+  b = a;
+
+  while (!a.IsEmpty()) {
+    CHECK_EQ(a.Top(), b.Top());
+    a.Pop();
+    b.Pop();
+  }
+
+  b.Push(first_complex);
+  b.Clear();
+  CHECK(b.IsEmpty());
+
+  CHECK_THROWS(a.Top());
 }
