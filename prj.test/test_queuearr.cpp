8aa111f9d3c3c1844670c6c2016db475bfcfa028
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-03-04 22:45:15 +0300

    tested: queuearr

--- prj.test/test_queuearr.cpp
+++ prj.test/test_queuearr.cpp
@@ -1,85 +1,71 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
 #include <queuearr/queuearr.hpp>
 
-TEST_CASE("base") {
-  QueueArr a;
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+TEST_CASE("Basic methods") {
+  QueueArr q1;
+  CHECK(q1.IsEmpty());
+  q1.Push(Complex(1, 1));
+  CHECK(!q1.IsEmpty());
+  q1.Pop();
+  CHECK(q1.IsEmpty());
+
+  QueueArr q2;
+  for (int i = 0; i < 3; i++) {
+    q1.Push(Complex(i, i));
+    q2.Push(Complex(i, i));
+    CHECK(q1.Top() == q2.Top());
+    q1.Pop(); q2.Pop();
+  }
+
+  CHECK(q1.IsEmpty());
+  CHECK(q2.IsEmpty());
+
+  for (int i = 1; i <= 3; i++) {
+    q1.Push(Complex(i, i * i));
+  }
+  q1.Pop();
+  q1.Pop();
+  q1.Push(Complex(5, 1));
   
-  Complex first_complex = { 3.4, 2.9 };
-  Complex second_complex = { 0,0 };
+  QueueArr q3(q1);
 
-  a.Push(first_complex);
-  CHECK_EQ(a.Top(), first_complex);
-  a.Push(second_complex);
-  a.Push(second_complex);
-  CHECK_EQ(a.Top(), first_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), second_complex);
-  a.Push(first_complex);
-  a.Push(first_complex);
-  a.Push(second_complex);
+  while (!q1.IsEmpty()) {
+    CHECK(q1.Top() == q3.Top());
+    CHECK(&q1.Top() != &q3.Top());
+    q1.Pop();
+    q3.Pop();
+  }
+  CHECK_THROWS(q1.Top());
+  CHECK_THROWS(q3.Top());
+}
 
-  a.Push(second_complex);
+TEST_CASE("Copying constructor and operator=") {
+  QueueArr q1;
+  QueueArr q2;
+  QueueArr q3;
+  q3.Push(Complex(1, 1));
 
-  CHECK_EQ(a.Top(), second_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), second_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), first_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), first_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), second_complex);
-  a.Pop();
-  CHECK_EQ(a.Top(), second_complex);
-  a.Pop();
-  CHECK(a.IsEmpty());
+  for (int i = 1; i <= 2; i++) {
+    q1.Push(Complex(i, i * i));
+    q2.Push(Complex(2 * i, 2 * i * 2 * i));
+  }
+  q2.Pop();
 
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Push(first_complex);
-  a.Push(second_complex);
-  QueueArr b(a);
 
-  while (!a.IsEmpty()) {
-    CHECK_EQ(a.Top(), b.Top());
-    a.Pop();
-    b.Pop();
+  q1 = q2;
+  QueueArr q4(q1);
+  while (!q1.IsEmpty()) {
+    CHECK(&q1.Top() != &q2.Top());
+    CHECK(q1.Top() == q2.Top());
+    q2.Pop(); q1.Pop();
   }
 
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Pop();
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Push(first_complex);
-  QueueArr c(a);
-
-  while (!a.IsEmpty()) {
-    CHECK_EQ(a.Top(), c.Top());
-    a.Pop();
-    c.Pop();
+  q3 = q4;
+  while (!q3.IsEmpty()) {
+    CHECK(&q3.Top() != &q4.Top());
+    CHECK(q3.Top() == q4.Top());
+    q3.Pop(); q4.Pop();
   }
-
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Pop();
-  a.Push(first_complex);
-  a.Push(second_complex);
-  a.Push(first_complex);
-  b = a;
-
-  while (!a.IsEmpty()) {
-    CHECK_EQ(a.Top(), b.Top());
-    a.Pop();
-    b.Pop();
-  }
-
-  b.Push(first_complex);
-  b.Clear();
-  CHECK(b.IsEmpty());
-
-  CHECK_THROWS(a.Top());
 }
