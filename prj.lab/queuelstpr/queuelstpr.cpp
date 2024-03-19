a15c672a942a4714ef63897ceb414cd1eb60f404
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-03-19 16:02:43 +0300

    upd: everything except copy ctor and operator=

--- prj.lab/queuelstpr/queuelstpr.cpp
+++ prj.lab/queuelstpr/queuelstpr.cpp
@@ -1,1 +1,75 @@
 #include <queuelstpr/queuelstpr.hpp>
+
+QueueLstPr::QueueLstPr(QueueLstPr&& rhs) noexcept
+  :head_(rhs.head_) {
+  rhs.head_ = nullptr;
+}
+
+QueueLstPr& QueueLstPr::operator=(QueueLstPr&& rhs) noexcept {
+  if (this != &rhs) {
+    std::swap(head_, rhs.head_);
+  }
+  return *this;
+}
+
+bool QueueLstPr::IsEmpty() const noexcept {
+  return head_ == nullptr;
+}
+
+void QueueLstPr::Clear() noexcept {
+  while (head_!=nullptr) {
+    this->Pop();
+  }
+}
+
+QueueLstPr::~QueueLstPr() {
+  this->Clear();
+}
+
+void QueueLstPr::Pop() noexcept {
+  if (head_ != nullptr) {
+    Node* tmp = head_->next_;
+    delete head_;
+    head_ = tmp;
+    tmp = nullptr;
+  }
+}
+
+float& QueueLstPr::Top() {
+  if (head_ == nullptr) {
+    throw std::logic_error("Queue Is Empty");
+  }
+  return head_->data_;
+}
+
+const float& QueueLstPr::Top() const{
+  if (head_ == nullptr) {
+    throw std::logic_error("Queue Is Empty");
+  }
+  return head_->data_;
+}
+
+void QueueLstPr::Push(const float& value) {
+  Node* New = new Node;
+  New->data_ = value;
+  
+  if (head_ == nullptr) {
+    head_ = New;
+    New = nullptr;
+  }
+  else {
+    Node* ptr = head_;
+    while (ptr->next_!=nullptr) {
+      ptr = ptr->next_;
+    }
+
+    if (ptr->next_ == nullptr) {
+      ptr->next_ = New;
+    }
+    else {
+      New->next_ = ptr->next_->next_;
+      ptr->next_ = New;
+    }
+  }
+  New = nullptr;
+}
---
+++ prj.lab/queuelstpr/queuelstpr.hpp
@@ -0,0 +1,38 @@
+
+
+
+#ifndef QUEUELSTPR_HPP
+#define QUEUELSTPR_HPP
+
+#include <algorithm>
+#include <stdexcept>
+
+class QueueLstPr {
+
+public:
+  QueueLstPr() = default;
+  QueueLstPr(const QueueLstPr& rhs);
+  QueueLstPr(QueueLstPr&& rhs) noexcept;
+  QueueLstPr& operator=(const QueueLstPr& rhs);
+  QueueLstPr& operator=(QueueLstPr&& rhs) noexcept;
+  ~QueueLstPr();
+
+  void Push(const float& value);
+  void Pop() noexcept;
+
+  float& Top();
+  const float& Top() const;
+
+  bool IsEmpty() const noexcept;
+  void Clear() noexcept;
+private:
+  struct Node {
+
+    Node* next_ = nullptr;
+    float data_ = 0;
+  };
+
+  Node* head_ = nullptr;
+};
+
+#endif // !QUEUELSTPR_HPP
