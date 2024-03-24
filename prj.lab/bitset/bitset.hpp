6816859f4fba77979ee409d85df6a82bbdfc66ba
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-03-24 16:40:57 +0300

    add: operators

--- prj.lab/bitset/bitset.hpp
+++ prj.lab/bitset/bitset.hpp
@@ -1,20 +1,22 @@
-
-
 #ifndef BITSET_HPP
 #define BITSET_HPP
 
+#include <iosfwd>
+#include <cstdint>
 #include <vector>
+
+const int32_t CEIL_NUM = 4294967296;
 
 class BitSet {
 
 public:
-  BitSet();
-  BitSet(const int32_t);
+  BitSet() = default;
+  BitSet(const int32_t cnt);
   BitSet(const BitSet& rhs);
-  BitSet(BitSet&& rhs);
+  BitSet(BitSet&& rhs) noexcept;
   BitSet& operator=(const BitSet& rhs);
-  BitSet& operator=(BitSet&& rhs);
-  ~BitSet();
+  BitSet& operator=(BitSet&& rhs) noexcept;
+  ~BitSet() = default;
 
   int32_t Size() const noexcept;
   void Resize(const int32_t size);
@@ -22,6 +24,15 @@ public:
   void Set(const int32_t idx, const bool value);
   bool Get(const int32_t idx);
 
+  BitSet& operator&=(const BitSet& rhs);
+  BitSet& operator|=(const BitSet& rhs);
+  BitSet& operator^=(const BitSet& rhs);
+  BitSet& operator~ ();
+
+  bool operator==(const BitSet& rhs);
+  bool operator!=(const BitSet& rhs);
+
+  void Arr();
   void Fill(const bool value);
 
 private:
