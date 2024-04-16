23754a94bbaf0f69bd6f5ae7a22567b7430f758d
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-04-16 14:07:03 +0300

    templated: stacklstt

--- prj.test/test_stacklstt.cpp
+++ prj.test/test_stacklstt.cpp
@@ -6,95 +6,94 @@
 #include <iostream>
 #include <chrono>
 
-
-using StackLst = StackLstT<Complex>;
-
-TEST_CASE("operator=") {
-  StackLst a;
-  Complex first = { 1,0 };
-  Complex sec = { 3,4 };
-  Complex third = { 0,0 };
-
-  a.Push(first);
-  a.Push(sec);
-  a.Push(third);
-
-  StackLst b;
-  b = a;
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
-
-  a.Push(first);
-  a.Push(first);
-  a.Push(first);
-  b.Push(first);
-  b.Push(sec);
-  b.Push(third);
-  b.Push(sec);
-  b = a;
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
-  CHECK_EQ(a.Top(), b.Top());
-  a.Pop();
-  b.Pop();
+template<class T>
+void get_values(std::vector<T>& values, int& size) {
+  for (int i = 0; i < 10; ++i) {
+    values.push_back(i);
+  }
+}
+template<> 
+void get_values(std::vector<std::string>& values, int& size) {
+  for (int i = 0; i < 10; ++i) {
+    values.push_back(std::to_string(i));
+  }
+}
+template<>
+void get_values(std::vector<bool>& values, int& size) {
+  for (int i = 0; i < 10; ++i) {
+    if (i % 2 == 0) {
+      values.push_back(1);
+    }
+    else {
+      values.push_back(0);
+    }
+  }
 }
 
 
-TEST_CASE("move semantics") {
-  StackLst a;
+TEST_CASE_TEMPLATE("based", T, int, double, Complex, std::string, bool) {
+  std::vector<T> values;
+  int size = 10;
+  get_values(values, size);
+  SUBCASE("operator=") {
+    StackLstT<T> a;
+    
+    for (int i = 0; i < values.size(); ++i) {
+      a.Push(values[i]);
+    }
 
-  float first_num = 0;
-  float second_num = 1.5;
-  float third_num = 3.0;
+    StackLstT<T> b;
+    b = a;
 
-  for (int i = 0; i < 10000; ++i) {
-    a.Push(first_num);
-    a.Push(second_num);
-    a.Push(third_num);
-  }
-  StackLst b;
-  b.Push(first_num);
-  b.Push(second_num);
-  b.Push(third_num);
-  StackLst c(a);
-  StackLst d(a);
-
-  auto start1 = std::chrono::high_resolution_clock::now();
-  b = std::move(a);
-  auto end1 = std::chrono::high_resolution_clock::now();
-
-  auto res1 = std::chrono::duration<float>{ end1 - start1 };
-
-  std::cout << res1 << '\n';
-
-  while (!c.IsEmpty()) {
-    CHECK_EQ(c.Top(), b.Top());
-    c.Pop();
-    b.Pop();
+    while (!a.IsEmpty()) {
+      CHECK_EQ(a.Top(), b.Top());
+      a.Pop();
+      b.Pop();
+    }
   }
 
-  auto start2 = std::chrono::high_resolution_clock::now();
-  a = d;
-  auto end2 = std::chrono::high_resolution_clock::now();
+  SUBCASE("move semantics") {
+    StackLstT<T> a;
 
-  auto res2 = std::chrono::duration<float>{ end2 - start2 };
 
-  std::cout << res2;
+    for (int i = 0; i < 10000; ++i) {
+      a.Push(values[i % 10]);
+      a.Push(values[i % 10]);
+    }
+    StackLstT<T> b;
+    b.Push(values[0]);
+    b.Push(values[1]);
+    b.Push(values[2]);
+    StackLstT<T> c(a);
+    StackLstT<T> d(a);
 
-  while (!d.IsEmpty()) {
-    CHECK_EQ(a.Top(), d.Top());
-    d.Pop();
-    a.Pop();
+    auto start1 = std::chrono::high_resolution_clock::now();
+    b = std::move(a);
+    auto end1 = std::chrono::high_resolution_clock::now();
+
+    auto res1 = std::chrono::duration<float>{ end1 - start1 };
+
+    std::cout << res1 << '\n';
+
+    while (!c.IsEmpty()) {
+      CHECK_EQ(c.Top(), b.Top());
+      c.Pop();
+      b.Pop();
+    }
+
+    auto start2 = std::chrono::high_resolution_clock::now();
+    a = d;
+    auto end2 = std::chrono::high_resolution_clock::now();
+
+    auto res2 = std::chrono::duration<float>{ end2 - start2 };
+
+    std::cout << res2;
+
+    while (!d.IsEmpty()) {
+      CHECK_EQ(a.Top(), d.Top());
+      d.Pop();
+      a.Pop();
+    }
   }
+  
 }
