e2433bc31a51527ff0be58a0731b74d87c493df1
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-04-20 15:56:47 +0300

    upd: gaussian filter

--- prj.cw/opencv.cpp
+++ prj.cw/opencv.cpp
@@ -1,34 +1,77 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <cmath>
+
+const double PI = acos(-1.0);
+
+
+double round_num(double num, int decimal) {
+  const double multip = std::pow(10.0, decimal);
+  return (std::round(num * multip) / multip);
+}
+
+double** GaussianMatrix(const int& length, const int& width, const double& sigma ) {
+  double** matrix = new double* [width];
+
+  for (size_t i = 0; i < width; ++i) {
+    double* row = new double[length];
+    matrix[i] = row;
+  }
+
+  for (size_t i = 0; i < width; ++i) {
+    for (size_t j = 0; j < length; ++j) {
+      matrix[i][j] = std::exp(-(std::abs(2-static_cast<int>(i)) * std::abs(2 - static_cast<int>(i)) + std::abs(2 - static_cast<int>(j)) * std::abs(2 - static_cast<int>(j))) / (2 * sigma * sigma))/(2*PI*sigma*sigma);
+      matrix[i][j] = round_num(matrix[i][j], 5);
+    }
+  }
+
+  return matrix;
+}
 
 
 int main() {
-  
-  cv::Mat img = cv::imread("D:/Desktop/data2.png");
+  //std::cout << std::fixed << std::setprecision(5);
+  double sigma = 1;
+  int matrix_lenght = 1 + 2 * ceil(2 * sigma);
+  double** weights = GaussianMatrix(matrix_lenght, matrix_lenght, sigma);
 
 
-  if (img.empty()) {
-    std::cout << "Failed";
-    std::cin.get();
-    return -1;
+  cv::Mat img = cv::imread("C:/Users/Huawei/Desktop/apple.png");
+  cv::Mat img_new = cv::imread("C:/Users/Huawei/Desktop/apple.png");
+
+  for (size_t i = matrix_lenght + 1; i < img.rows-2; ++i) {
+    for (size_t j = matrix_lenght + 1; j < img.cols-2; ++j) {
+      
+      uchar b_sum = 0;
+      uchar g_sum = 0;
+      uchar r_sum = 0;
+      for (size_t i_ = i - matrix_lenght; i_ <= i + matrix_lenght; ++i_) {
+        for (size_t j_ = j - matrix_lenght; j_ <= j + matrix_lenght; ++j_) {
+          cv::Vec3b& BGR = img.at<cv::Vec3b>(i_, j_);
+          b_sum += BGR[0] * weights[i_ % (i - matrix_lenght/2)][j_ % (j - matrix_lenght/2)];
+          g_sum += BGR[1] * weights[i_ % (i - matrix_lenght / 2)][j_ % (j - matrix_lenght / 2)];
+          r_sum += BGR[2] * weights[i_ % (i - matrix_lenght / 2)][j_ % (j - matrix_lenght / 2)];
+        }
+      }
+      cv::Vec3b& pix = img_new.at<cv::Vec3b>(i, j);
+      pix[0] = b_sum;
+      pix[1] = g_sum;
+      pix[2] = r_sum;
+
+    }
+    std::cout << '\n';
   }
-  
-  cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
 
-  cv::Mat new_img;
-  cv::equalizeHist(img, new_img);
+  cv::String window_name1 = "old_img";
+  cv::String window_name2 = "new_img";
 
-  cv::String windowNameOldPic = "Old Img";
-  cv::String windowNameNewPic = "New Img";
+  cv::namedWindow(window_name1);
+  cv::namedWindow(window_name2);
 
-  cv::namedWindow(windowNameNewPic);
-  cv::namedWindow(windowNameOldPic);
-
-  cv::imshow(windowNameOldPic, img);
-  cv::imshow(windowNameNewPic, new_img);
-
+  cv::imshow(window_name1, img);
+  cv::imshow(window_name2, img_new);
   cv::waitKey(0);
 
-  cv::destroyAllWindows();
+
 
 }
