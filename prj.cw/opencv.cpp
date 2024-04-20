ca70a12cb486d9a8c6dc2a47b0bca998dd57fd87
Huawei <Huawei@LAPTOP-EOG5GJRT>
2024-04-21 00:15:03 +0300

    upd: gaussian blur

--- prj.cw/opencv.cpp
+++ prj.cw/opencv.cpp
@@ -10,17 +10,17 @@ double round_num(double num, int decimal) {
   return (std::round(num * multip) / multip);
 }
 
-double** GaussianMatrix(const int& length, const int& width, const double& sigma ) {
-  double** matrix = new double* [width];
+double** GaussianMatrix(const int& length,  const double& sigma ) {
+  double** matrix = new double* [length];
 
-  for (size_t i = 0; i < width; ++i) {
+  for (size_t i = 0; i < length; ++i) {
     double* row = new double[length];
     matrix[i] = row;
   }
 
-  for (size_t i = 0; i < width; ++i) {
+  for (size_t i = 0; i < length; ++i) {
     for (size_t j = 0; j < length; ++j) {
-      matrix[i][j] = std::exp(-(std::abs(2-static_cast<int>(i)) * std::abs(2 - static_cast<int>(i)) + std::abs(2 - static_cast<int>(j)) * std::abs(2 - static_cast<int>(j))) / (2 * sigma * sigma))/(2*PI*sigma*sigma);
+      matrix[i][j] = std::exp(-(std::abs(length/2-static_cast<int>(i)) * std::abs(length / 2 - static_cast<int>(i)) + std::abs(length / 2 - static_cast<int>(j)) * std::abs(length / 2 - static_cast<int>(j))) / (2 * sigma * sigma))/(2*PI*sigma*sigma);
       matrix[i][j] = round_num(matrix[i][j], 5);
     }
   }
@@ -31,22 +31,23 @@ double** GaussianMatrix(const int& length, const int& width, const double& sigma
 
 int main() {
   //std::cout << std::fixed << std::setprecision(5);
+
   double sigma = 1;
   int matrix_lenght = 1 + 2 * ceil(2 * sigma);
-  double** weights = GaussianMatrix(matrix_lenght, matrix_lenght, sigma);
+  double** weights = GaussianMatrix(matrix_lenght, sigma);
 
 
-  cv::Mat img = cv::imread("C:/Users/Huawei/Desktop/apple.png");
-  cv::Mat img_new = cv::imread("C:/Users/Huawei/Desktop/apple.png");
+  cv::Mat img = cv::imread("C:/Users/Huawei/Desktop/mona.jpg");
+  cv::Mat img_new = cv::imread("C:/Users/Huawei/Desktop/mona.jpg");
 
-  for (size_t i = matrix_lenght + 1; i < img.rows-2; ++i) {
-    for (size_t j = matrix_lenght + 1; j < img.cols-2; ++j) {
+  for (size_t i = matrix_lenght + 1; i < img.rows-matrix_lenght/2; ++i) {
+    for (size_t j = matrix_lenght + 1; j < img.cols-matrix_lenght/2; ++j) {
       
       uchar b_sum = 0;
       uchar g_sum = 0;
       uchar r_sum = 0;
-      for (size_t i_ = i - matrix_lenght; i_ <= i + matrix_lenght; ++i_) {
-        for (size_t j_ = j - matrix_lenght; j_ <= j + matrix_lenght; ++j_) {
+      for (size_t i_ = i - matrix_lenght/2; i_ <= i + matrix_lenght/2; ++i_) {
+        for (size_t j_ = j - matrix_lenght/2; j_ <= j + matrix_lenght/2; ++j_) {
           cv::Vec3b& BGR = img.at<cv::Vec3b>(i_, j_);
           b_sum += BGR[0] * weights[i_ % (i - matrix_lenght/2)][j_ % (j - matrix_lenght/2)];
           g_sum += BGR[1] * weights[i_ % (i - matrix_lenght / 2)][j_ % (j - matrix_lenght / 2)];
