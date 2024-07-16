11b699804637ad24d51d8f7e4f48e281519a6432
beastcris <144485942+beastcris@users.noreply.github.com>
2024-07-17 00:19:19 +0300

    upd: robobuba.cpp

--- prj.app/robobuba/robobuba.cpp
+++ prj.app/robobuba/robobuba.cpp
@@ -1,20 +1,20 @@
 #include "robobuba.hpp"
 
-void Right::execute(int value, Buffer& buf) {
-  buf.idx = (buf.idx + value) % buf.values.size();
+void Right::execute(Buffer& buf) {
+  buf.idx = (buf.idx + this->value_) % buf.values.size();
 }
 
-void Left::execute(int value, Buffer& buf) {
-  buf.idx = (buf.idx - value) % buf.values.size();
+void Left::execute(Buffer& buf) {
+  buf.idx = (buf.idx - this->value_) % buf.values.size();
   if (buf.idx < 0) {
     buf.idx = buf.values.size() - buf.idx;
   }
 }
 
-void Add::execute(int value, Buffer& buf) {
-  buf.values[buf.idx] += value;
+void Add::execute(Buffer& buf) {
+  buf.values[buf.idx] += this->value_;
 }
 
-void Sub::execute(int value, Buffer& buf) {
-  buf.values[buf.idx] -= value;
+void Sub::execute(Buffer& buf) {
+  buf.values[buf.idx] -= this->value_;
 }
